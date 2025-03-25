17d930b25426db808c3c6248a432faf2b9fbdb92
aashikur <mdaashikur@gmail.com>
2025-03-26 03:09:54 +0600

    update All Section

--- script.js
+++ script.js
@@ -28,3 +28,72 @@ handleCarouselState(prefersReducedMotion.matches);
 
 // Listen for preference changes
 prefersReducedMotion.addEventListener('change', (e) => handleCarouselState(e.matches));
+
+
+
+
+function first(){
+    const first = document.getElementById('first');
+    const secound = document.getElementById('secound');
+    const secondBtn = document.getElementById('secondBtn');
+
+    first.style.display = 'none';
+    setTimeout(()=>{
+        secound.classList.remove('hidden')
+        secound.classList.add('animate-pop2')
+    },400)
+
+
+     setTimeout(()=>{
+        secondBtn.classList.remove('hidden');
+        secondBtn.classList.add('animate-pop2');
+     },10)
+}
+
+
+function second(){
+    document.getElementById('secound').classList.add('hidden'); third
+
+    document.getElementById('third').classList.remove('hidden'); 
+    document.getElementById('third').classList.add('animate-pop2'); 
+
+}
+
+
+function cakeEat(){
+    document.getElementById('cake1').classList.remove('hidden')
+    document.getElementById('cake2').classList.add('hidden');
+    setTimeout(()=>{
+        document.getElementById('third').classList.add('hidden');
+        document.getElementById('sms').classList.remove('hidden');
+
+        setTimeout(()=>{
+            const thisText =  document.getElementById('text');
+
+             let smallSMS = 'ThanQ For Everything. May Allah Grand your deepest wish in this Ramadan.'
+             smallSMS = smallSMS.split('');
+             console.log(smallSMS);
+             let i = 0; 
+             let end = smallSMS.length;
+            setInterval(()=>{
+
+                if(i ==0 ) {
+                    thisText.innerHTML = '';
+                    thisText.classList.remove('wish');
+
+                }
+                if(i<end)
+               {
+                smallSMS.forEach(txt => {
+                    thisText.innerHTML += txt;
+                })
+                
+               }
+
+
+            },100);
+        },3000);
+    },2000)
+}
+
+
