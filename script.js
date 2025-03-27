4a51ab5edaa6db97ae6e1272f9f5d8dc0c00ffbe
aashikur <mdaashikur@gmail.com>
2025-03-28 02:53:09 +0600

    almost

--- script.js
+++ script.js
@@ -34,20 +34,44 @@ prefersReducedMotion.addEventListener('change', (e) => handleCarouselState(e.mat
 
 document.getElementById('F_btn').addEventListener('click',()=>{
 
+    const counter = document.getElementById('counter');
+    let count = 9;
+
     const first = document.getElementById('first');
     const secound = document.getElementById('secound');
     const secondBtn = document.getElementById('secondBtn');
 
     first.style.display = 'none';
     setTimeout(()=>{
+   
         secound.classList.remove('hidden')
         secound.classList.add('animate-pop2')
-    },400)
+        const intervalId = setInterval(() => {
+          counter.innerText = count--;
+        }, 1000);
+
+        setTimeout(() => {
+          clearInterval(intervalId);
+        }, 10000);
+    },0)
 
 
      setTimeout(()=>{
         secondBtn.classList.remove('hidden');
         secondBtn.classList.add('animate-pop2');
+
+
+        const p1 = document.getElementById('counter');
+        const p2 = document.getElementById('more');
+        p2.classList.add('hidden')
+
+        setTimeout(()=>{
+
+          p1.classList.add('hidden')
+          p2.classList.remove('hidden')
+
+        },10000)
+
      },10)
 
 }) 
@@ -62,7 +86,7 @@ function second(){
 
 }
 
-
+let SintervalID = 0;
 function cakeEat(){
     document.getElementById('cake1').classList.remove('hidden')
     document.getElementById('cake2').classList.add('hidden');
@@ -70,21 +94,29 @@ function cakeEat(){
         document.getElementById('third').classList.add('hidden');
         document.getElementById('sms').classList.remove('hidden');
 
+      setTimeout(()=>{
+        document.getElementById('Random').classList.remove('hidden');
+      },3000)
+
+
         setTimeout(()=>{
             const thisText =  document.getElementById('text');
 
              let smallSMS = 'ThanQ For Everything. May Allah Grand your deepest wish in this Ramadan.'
              smallSMS = smallSMS.split('');
-             console.log(smallSMS);
+            //  console.log(smallSMS);
              let i = 0; 
              let end = smallSMS.length;
-            setInterval(()=>{
+             SintervalID = setInterval(()=>{
+
+
 
                 if(i ==0 ) {
                     thisText.innerHTML = '';
                     thisText.classList.remove('wish');
 
                 }
+                
                 if(i<end)
                {
                 smallSMS.forEach(txt => {
@@ -95,8 +127,54 @@ function cakeEat(){
 
 
             },100);
+            
+             setTimeout(()=>{
+              clearInterval(SintervalID);
+             },110)
+            
+
         },3000);
     },2000)
 }
 
 
+
+function Random(){
+  const thisText =  document.getElementById('text');
+  const randomNumber = Math.floor(Math.random() * 19);
+  const motivationalWishes = [
+    // "Keep going—you're closer than you think!",
+    // "Every step you take brings you closer to your dreams.",
+    // "You are capable of achieving greatness, one day at a time.",
+    // "Believe in yourself; you've got this!",
+    // "Every challenge is an opportunity to grow.",
+    // "Stay focused and keep pushing forward—success is on the horizon.",
+    // "You are stronger than you realize.",
+    // "You are stronger than you realize.",
+    // "Keep dreaming big, and make it happen.",
+    // "Your hard work will pay off—stay persistent.",
+    // "Believe in progress, not perfection. You're on the right track!",
+    'If you continue to wait for the right time, you will "Waste" your "entire" life, and nothing will happen.',
+    "Work hard So hard that, they call it talent. ",
+    'You’ll be much happier if you forgive your parents and stop blaming them for your problems',
+    "You don't need 100 self-help books. All you need is action. Plus, self-discipline",
+    'The most difficult mission on earth is to focus on your dreams, and the easiest "TASK" is to complain',
+    "It's Easy to Have Faith when everything going smoothly. But remember It's that Strom, that test the strongest sailor. Will you be Standing?",
+    "Who am I to judge another when I myself walk as an imperfect",
+    "People's always looking for the right person but no one want to be the right person.",
+    "Most beautifull persons is with rich personally and Mindset; which Remain eternal while looks is just temporary. ",
+    "Nothing is better than memories, and nothing is worse than them.",
+    "You can not control your heart; but you can control your action.",
+    "Make Mistakes,  but don't regrate them, because regrate is the hardest thing to carry when you get older. ",
+    "What he lost that he Found Allah, and what he found that he lost Allah.",
+    "Silence doesn't always mean you have nothing to say. It may mean you realize that no matter what you say, it won't change anything.",
+    "Just because you lost me as a friend doesn't mean you gained me as an enemy. I'm bigger than that. I still wanna see you eat, just not at my table",
+    "Every single good thing and bad thing will lead you to where you're meant to be",
+    "Sometimes, you need to listen to your own advice. You always know what to do but can't decide.",
+    "You will understand the true meaning of life when reality hits you.",
+
+  ];
+
+  console.log(motivationalWishes.length)
+  thisText.innerHTML = motivationalWishes[randomNumber];
+}
